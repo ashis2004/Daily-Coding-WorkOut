3248bb402bd35c50abe07e41c0ca1e1fbdd61cdd
Ashish Kumar Patel <110098153+ashis2004@users.noreply.github.com>
2025-10-07 18:27:05 +0530

    Time: 16 ms (10.59%) | Memory: 45.2 MB (45.88%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,25 +1,23 @@
 class Solution {
     public String convert(String s, int numRows) {
-       String[] str = new String[numRows];
-       for (int i = 0; i < numRows; i++) {
-           str[i] = ""; // Initialize as empty string
+       String [] str=new String[numRows];
+       for(int i=0;i<str.length;i++){
+        str[i]="";
        }
-
-       int j = 0;
-       while (j < s.length()) {
-           for (int i = 0; i < numRows && j < s.length(); i++) {
-               str[i] += s.charAt(j++);
-           }
-
-           for (int i = numRows - 2; i > 0 && j < s.length(); i--) {
-               str[i] += s.charAt(j++);
-           }
+       int j=0;
+       while(j<s.length()){
+        for(int i=0;i<numRows && j<s.length();i++){
+            str[i]+=s.charAt(j++);
+        }
+        for(int i=numRows-2;i>0&&j<s.length();i--){
+            str[i]+=s.charAt(j++);
+        }
+       
        }
-
-       String ans = "";
-       for (String res : str) {
-           ans += res; // Corrected concatenation
-       }
-       return ans;
+        String res="";
+        for(String st:str){
+            res+=st;
+        }
+        return res.toString();
     }
 }
