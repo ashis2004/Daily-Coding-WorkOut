94bb65255d168d2af9ac97d1a4ae326a00ca7468
Ashish Kumar Patel <110098153+ashis2004@users.noreply.github.com>
2025-10-07 17:52:19 +0530

    Time: 1 ms (84.92%) | Memory: 41.3 MB (12.92%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+      long start=1;
+        long end=x;
+        long ans=0;
+        
+        while(start<=end){
+            long mid=start+(end-start)/2;
+
+            if(mid*mid==x){  // perfect squre
+                ans=(int)mid;
+                break;
+                
+            }else if(mid*mid<x){ //
+                start=mid+1;
+                ans=mid;
+            }else{
+                end=mid-1;
+            }
+            
+
+
+        }
+        return (int)ans;
+        
+    }
+}
+
