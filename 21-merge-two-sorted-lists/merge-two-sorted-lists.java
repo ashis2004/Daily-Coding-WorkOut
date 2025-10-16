bb202a7f620ea064e195f5e977fbc011b6491b3c
Ashish Kumar Patel <110098153+ashis2004@users.noreply.github.com>
2025-10-16 15:47:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (47.35%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -11,32 +11,15 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         
-        if(list1==null) return list2;
-        if(list2==null) return list1;
-        
-        if(list1.val<list2.val){
-            list1.next=mergeTwoLists(list1.next,list2);
-            return list1;
-        }
-        else{
-            list2.next=mergeTwoLists(list2.next,list1);
-            return list2;
-        }
-//         //Approach: Using recursion
-
-//         //base case
-//         if(list1==null) return list2;
-//         if(list2==null) return list1;
-
-//         //check condition if value of list1 is less than value of list2,
-//         //than we recursively add whole list2 in to list1
-//         if(list1.val<list2.val){
-//             list1.next = mergeTwoLists(list1.next, list2);
-//             return list1;
-//         }
-//         else{
-//             list2.next = mergeTwoLists(list1, list2.next);
-//             return list2;
-//         }
+       if(list1==null) return list2;
+       if(list2==null) return list1;
+       if(list1.val<list2.val) {
+        list1.next=mergeTwoLists(list1.next,list2);
+        return list1;
+       }
+       else{
+        list2.next=mergeTwoLists(list2.next,list1);
+        return list2;
+       }
     }
 }
